b1323b5d110792ad60660651e1d104f7c9c2e572
sdott3 <sdotthomas347@gmail.com>
2022-10-02 16:43:00 -0400

    updated things to do controller

--- server/controllers/ThingsToDo.js
+++ server/controllers/ThingsToDo.js
@@ -1,3 +1,13 @@
+import React, { useState} from "react";
 
-const ThingsToDo = require('../models/ThingsToDo');
-const mongoose = require('mongoose')
+function App () {
+    function apiUrl = 'http://api.opentripmap.com/0.1/ru/places/xid/Q372040?apikey=5ae2e3f221c38a28845f05b6b28dd69575b483ead24be6a1f3db9410'
+
+
+    function ThingsToDo() {
+        fetch(apiURL)
+            .then(resp => resp.json())
+            .then(data =>)
+    }
+        
+}
