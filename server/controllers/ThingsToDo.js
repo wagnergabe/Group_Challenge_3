28e9796eb73565494bfffd4e5fd18ea6038aef86
sdott3 <sdotthomas347@gmail.com>
2022-10-02 16:58:22 -0400

    updated things to do controller

--- server/controllers/ThingsToDo.js
+++ server/controllers/ThingsToDo.js
@@ -1,13 +1,20 @@
 import React, { useState} from "react";
 
-function App () {
-    function apiUrl = 'http://api.opentripmap.com/0.1/ru/places/xid/Q372040?apikey=5ae2e3f221c38a28845f05b6b28dd69575b483ead24be6a1f3db9410'
+const App = () => {
+    const [user, setUsers] = useState([])
 
 
-    function ThingsToDo() {
-        fetch(apiURL)
-            .then(resp => resp.json())
-            .then(data =>)
+    const  ThingsToDo = () => {
+        fetch("http://api.opentripmap.com/0.1/ru/places/xid/Q372040?apikey=5ae2e3f221c38a28845f05b6b28dd69575b483ead24be6a1f3db9410")
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setUsers(data)
+            })
+
     }
         
 }
+
+export default App;
