86fd2bfb22376d653c38370d4dbb532ea118f5d4
Miguel Soriano <originalsorianom@gmail.com>
2022-10-04 04:26:54 -0700

    update signup.js in pages

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,3 +3,31 @@ import { Link } from 'react-router-dom';
 import {useMutation} from '@apollo/client';
 import auth from '../utils/auth';
 import {ADD_USER} from '../utils/mutations';
+
+function Signup(props) {
+    const [formState, setFormState] = useState({username:'', email:'', password:''});
+    const [addUser] =useMutation(ADD_USER);
+    
+    //submit form
+    const handleFormSubmit = async(event) => {
+        event.preventDefault();
+
+        const mutationResponse = await addUser({
+        variables: {username:formState.username  ,email: formState.email, password: formState.password},
+            });
+            const token = mutationResponse.data.addUser.token;
+            auth.login(token);
+      
+    };
+// update state on form input
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+//borrow from module 22 
+
+
