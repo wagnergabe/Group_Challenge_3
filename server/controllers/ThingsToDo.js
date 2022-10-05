cee2adbd44ff6d6288a4d7e26e7610f9eedf6c81
sdott3 <sdotthomas347@gmail.com>
2022-10-04 20:09:14 -0400

    updating files

--- server/controllers/ThingsToDo.js
+++ server/controllers/ThingsToDo.js
@@ -1,11 +1,11 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-    const [user, setUsers] = useState([])
+    const [user, setUsers] = useState([]);
 
 
     const  ThingsToDo = () => {
-        fetch("http://api.opentripmap.com/0.1/ru/places/xid/Q372040?apikey=5ae2e3f221c38a28845f05b6b28dd69575b483ead24be6a1f3db9410")
+        fetch("https://api.opentripmap.com/0.1/en/places/geoname?name=cityName&country=us&apikey=5ae2e3f221c38a28845f05b6b28dd69575b483ead24be6a1f3db9410")
             .then(response => {
                 return response.json()
             })
