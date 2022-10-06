d07d32c7984d5b1c0d040955eaf8c5e9db059e17
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2022-10-06 16:53:36 -0500

    update index

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/deep-thoughts",
+  process.env.MONGO_URI || "mongodb+srv://bootcamp:test@groupproject3.vqiizxt.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
